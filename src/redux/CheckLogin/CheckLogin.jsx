c4a12d2fee2bb86e5ddcc6680b98698cad8f44ee
buidung <dung0136vh@gmail.com>
2025-10-28 14:11:19 +0700

    update

--- src/redux/CheckLogin/CheckLogin.jsx
+++ src/redux/CheckLogin/CheckLogin.jsx
@@ -1,11 +1,29 @@
 import { Navigate, Outlet } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 import getCookie from '~/utils/getcookie'
+import { getUserInfoAPI } from '~/apis/auth'
+import { setUserInfo } from '~/redux/features/comon'
 
 const useAuth = () => {
-  const accessToken = getCookie('accessToken')
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.comon.user)
   const refreshToken = getCookie('refreshToken')
-  // Đảm bảo có cả 2 token để xác định user đã đăng nhập
-  return !!(accessToken && refreshToken)
+
+  useEffect(() => {
+    if (refreshToken && !user.userId) {
+      getUserInfoAPI()
+        .then((userInfo) => {
+          dispatch(setUserInfo(userInfo))
+        })
+        .catch(() => {
+          document.cookie = 'refreshToken=; path=/; max-age=0'
+          document.cookie = 'accessToken=; path=/; max-age=0'
+        })
+    }
+  }, [refreshToken, user.userId, dispatch])
+
+  return !!refreshToken
 }
 
 export const PublicRoute = () => {
